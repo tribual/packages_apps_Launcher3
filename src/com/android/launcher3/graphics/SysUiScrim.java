ae424ce09a62e5d9fe30c0390c3a105bf24ec326
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-07 18:26:16 +0530

    Launcher3: Allow hiding top shadow on statusbar
    On certain devices (especially amoled) with certain
    black-themed wallpapers, it looks quite bad. Add a
    toggle to disable it, inspired from lawnchair:
    
    https://github.com/LawnchairLauncher/lawnchair/commit/c99e61205cc4d5b5d3d9b827787c9fd873616060
    https://github.com/LawnchairLauncher/lawnchair/commit/e41e27a4d11e7f8352a3d2fa74afb04e8af9354a
    
    Co-authored-by: Suphon Thanakornpakapong <paphonb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/graphics/SysUiScrim.java
+++ src/com/android/launcher3/graphics/SysUiScrim.java
@@ -21,6 +21,7 @@ import static android.graphics.Paint.FILTER_BITMAP_FLAG;
 import static com.android.launcher3.config.FeatureFlags.KEYGUARD_ANIMATION;
 
 import android.animation.ObjectAnimator;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.LinearGradient;
@@ -36,6 +37,7 @@ import androidx.annotation.VisibleForTesting;
 
 import com.android.launcher3.BaseDraggingActivity;
 import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.anim.AnimatedFloat;
 import com.android.launcher3.testing.shared.ResourceUtils;
@@ -46,7 +48,8 @@ import com.android.launcher3.util.Themes;
 /**
  * View scrim which draws behind hotseat and workspace
  */
-public class SysUiScrim implements View.OnAttachStateChangeListener {
+public class SysUiScrim implements View.OnAttachStateChangeListener,
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     /**
      * Receiver used to get a signal that the user unlocked their device.
@@ -73,6 +76,8 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
     private static final int BOTTOM_MASK_HEIGHT_DP = 200;
     private static final int TOP_MASK_HEIGHT_DP = 100;
 
+    private static final String KEY_SHOW_TOP_SHADOW = "pref_show_top_shadow";
+
     private boolean mDrawTopScrim, mDrawBottomScrim;
 
     private final RectF mTopMaskRect = new RectF();
@@ -98,10 +103,13 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
         mRoot = view;
         mActivity = BaseDraggingActivity.fromContext(view.getContext());
         DisplayMetrics dm = mActivity.getResources().getDisplayMetrics();
+        SharedPreferences prefs = LauncherPrefs.getPrefs(view.getContext());
+        prefs.registerOnSharedPreferenceChangeListener(this);
 
         mTopMaskHeight = ResourceUtils.pxFromDp(TOP_MASK_HEIGHT_DP, dm);
         mBottomMaskHeight = ResourceUtils.pxFromDp(BOTTOM_MASK_HEIGHT_DP, dm);
-        mHideSysUiScrim = Themes.getAttrBoolean(view.getContext(), R.attr.isWorkspaceDarkText);
+        mHideSysUiScrim = Themes.getAttrBoolean(view.getContext(), R.attr.isWorkspaceDarkText) &&
+                                prefs.getBoolean(KEY_SHOW_TOP_SHADOW, true);
 
         mTopMaskBitmap = mHideSysUiScrim ? null : createDitheredAlphaMask(mTopMaskHeight,
                 new int[]{0x50FFFFFF, 0x0AFFFFFF, 0x00FFFFFF},
@@ -181,6 +189,13 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
     @Override
     public void onViewDetachedFromWindow(View view) {
         ScreenOnTracker.INSTANCE.get(mActivity).removeListener(mScreenOnListener);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(KEY_SHOW_TOP_SHADOW)) {
+            mRoot.invalidate();
+        }
     }
 
     /**
