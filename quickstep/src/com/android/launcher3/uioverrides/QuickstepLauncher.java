15abb354bd630048f768fc7b7c2c55de7c29ee71
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-17 00:09:38 +0800

    Launcher3: Launch apps in freeform mode system shortcut
    Change-Id: I3b68d38509cbe45890bfab49435be2e08ea04e26
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -38,6 +38,7 @@ import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCH
 import static com.android.launcher3.model.data.ItemInfo.NO_MATCHING_ID;
 import static com.android.launcher3.popup.QuickstepSystemShortcut.getSplitSelectShortcutByPosition;
 import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
+import static com.android.launcher3.popup.SystemShortcut.FREE_FORM;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.UNINSTALL;
 import static com.android.launcher3.popup.SystemShortcut.WIDGETS;
@@ -429,6 +430,7 @@ public class QuickstepLauncher extends Launcher {
         List<SystemShortcut.Factory> shortcuts = new ArrayList(Arrays.asList(
                 APP_INFO, WellbeingModel.SHORTCUT_FACTORY, mHotseatPredictionController));
         shortcuts.addAll(getSplitShortcuts());
+        shortcuts.add(FREE_FORM);
         shortcuts.add(UNINSTALL);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
