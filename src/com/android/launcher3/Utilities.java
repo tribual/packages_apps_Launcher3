f3537045f163da9377f426b54d226552020648be
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-09 14:42:41 +0800

    Launcher3: Fix default overview blur level
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1041,7 +1041,7 @@ public final class Utilities {
     public static int getBlurRadius(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getInt(KEY_BLUR_DEPTH,
-                (int) context.getResources().getDimension(R.dimen.max_depth_blur_radius));
+                    context.getResources().getInteger(R.integer.max_depth_blur_radius));
     }
 
     public static boolean isShortParallax(Context context) {
