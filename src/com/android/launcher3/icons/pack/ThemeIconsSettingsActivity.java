ProjectMatrixx/android_packages_apps_Launcher3

Launcher3: Integrate themed icons picker to Launcher3
Change-Id: I666cfd9c6164f85109643ea012e7a4ee525fdfbd
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: mukesh22584 <mks22584@gmail.com>

src/com/android/launcher3/icons/pack/ThemeIconsSettingsActivity.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.icons.pack;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragment.OnPreferenceStartFragmentCallback;
+import androidx.preference.PreferenceFragment.OnPreferenceStartScreenCallback;
+import androidx.preference.PreferenceScreen;
+
+import com.android.launcher3.R;
+import com.android.launcher3.util.PackageManagerHelper;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class ThemeIconsSettingsActivity extends CollapsingToolbarBaseActivity implements
+        OnPreferenceStartFragmentCallback, OnPreferenceStartScreenCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (savedInstanceState == null) {
+            final Fragment f = Fragment.instantiate(this,
+                getString(R.string.themed_icon_pack_settings_class), null);
+            // Display the fragment as the main content.
+            getFragmentManager().beginTransaction()
+                    .replace(com.android.settingslib.widget.R.id.content_frame, f)
+                    .commit();
+        }
+    }
+
+    private boolean startFragment(String fragment, Bundle args, String key) {
+        if (getFragmentManager().isStateSaved()) {
+            // Sometimes onClick can come after onPause because of being posted on the handler.
+            // Skip starting new fragments in that case.
+            return false;
+        }
+
+        final Fragment f = Fragment.instantiate(this, fragment, args);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(com.android.settingslib.widget.R.id.content_frame, f)
+                .addToBackStack(key)
+                .commit();
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(PreferenceFragment preferenceFragment,
+            Preference pref) {
+        return startFragment(pref.getFragment(), pref.getExtras(), pref.getKey());
+    }
+
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragment caller, PreferenceScreen pref) {
+        Bundle args = new Bundle();
+        args.putString(PreferenceFragment.ARG_PREFERENCE_ROOT, pref.getKey());
+        return startFragment(getString(R.string.themed_icon_pack_settings_class),
+                args, pref.getKey());
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_icon_pack, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (id == R.id.menu_icon_pack) {
+            return openMarket();
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private boolean openMarket() {
+        final String query = getString(R.string.icon_pack_title);
+        final Intent intent = PackageManagerHelper.getMarketSearchIntent(this, query);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            Toast.makeText(this, R.string.icon_pack_no_market, Toast.LENGTH_LONG)
+                .show();
+            return false;
+        }
+
+        startActivity(intent);
+        return true;
+    }
+}

tribual/packages_apps_Launcher3

src/com/android/launcher3/icons/pack/ThemeIconsSettingsActivity.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.icons.pack;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.net.Uri;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragment.OnPreferenceStartFragmentCallback;
+import androidx.preference.PreferenceFragment.OnPreferenceStartScreenCallback;
+import androidx.preference.PreferenceScreen;
+
+import com.android.launcher3.R;
+import com.android.launcher3.util.PackageManagerHelper;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class ThemeIconsSettingsActivity extends CollapsingToolbarBaseActivity implements
+        OnPreferenceStartFragmentCallback, OnPreferenceStartScreenCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (savedInstanceState == null) {
+            final Fragment f = Fragment.instantiate(this,
+                getString(R.string.themed_icon_pack_settings_class), null);
+            // Display the fragment as the main content.
+            getFragmentManager().beginTransaction()
+                    .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, f)
+                    .commit();
+        }
+    }
+
+    private boolean startFragment(String fragment, Bundle args, String key) {
+        if (getFragmentManager().isStateSaved()) {
+            // Sometimes onClick can come after onPause because of being posted on the handler.
+            // Skip starting new fragments in that case.
+            return false;
+        }
+
+        final Fragment f = Fragment.instantiate(this, fragment, args);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, f)
+                .addToBackStack(key)
+                .commit();
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(PreferenceFragment preferenceFragment,
+            Preference pref) {
+        return startFragment(pref.getFragment(), pref.getExtras(), pref.getKey());
+    }
+
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragment caller, PreferenceScreen pref) {
+        Bundle args = new Bundle();
+        args.putString(PreferenceFragment.ARG_PREFERENCE_ROOT, pref.getKey());
+        return startFragment(getString(R.string.themed_icon_pack_settings_class),
+                args, pref.getKey());
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_icon_pack, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (id == R.id.menu_icon_pack) {
+            return openSampleUrl();
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private boolean openSampleUrl() {
+        final String url = "https://github.com/WaifuPX-DG/DGicons/releases";
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            Toast.makeText(this, R.string.icon_pack_no_browser, Toast.LENGTH_LONG)
+                .show();
+            return false;
+        }
+
+        startActivity(intent);
+        return true;
+    }
+}